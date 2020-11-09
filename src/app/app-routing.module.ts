74ac3fd6cbfa170aee5029c45fc9aa563804382c
Cuong <bdcuong91@gmail.com>
2020-11-09 19:32:31 +0100

    initializing app with components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [];
-
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  {
+    path: "customers",
+    loadChildren: "../app/customers/customers.module#CustomersModule"
+  }
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [CommonModule, RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
