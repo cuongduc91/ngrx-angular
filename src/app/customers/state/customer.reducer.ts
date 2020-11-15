857a7a32a837c8d7f4d6a3b7ec1ed77c9d721426
Cuong <bdcuong91@gmail.com>
2020-11-15 15:21:16 +0100

    add selector

--- src/app/customers/state/customer.reducer.ts
+++ src/app/customers/state/customer.reducer.ts
@@ -1,7 +1,7 @@
 import * as customerActions from "./customer.actions";
 import { Customer } from "../customer.model";
 import * as fromRoot from "../../state/app-state";
-
+import { createFeatureSelector, createSelector } from "@ngrx/store";
 export interface CustomerState {
   customers: Customer[];
   loading: boolean;
@@ -54,3 +54,27 @@ export function customerReducer(
     }
   }
 }
+const getCustomerFeatureState = createFeatureSelector<CustomerState>(
+  "customers"
+);
+
+export const getCustomers = createSelector(
+  getCustomerFeatureState,
+  (state: CustomerState) => state.customers
+);
+
+export const getCustomersLoading = createSelector(
+  getCustomerFeatureState,
+  (state: CustomerState) => state.loading
+);
+
+export const getCustomersLoaded = createSelector(
+  getCustomerFeatureState,
+  (state: CustomerState) => state.loaded
+);
+
+export const getError = createSelector(
+  getCustomerFeatureState,
+  (state: CustomerState) => state.error
+);
+
